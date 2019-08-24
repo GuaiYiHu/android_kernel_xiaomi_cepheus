d4515f184e5b98722e9f4d6218d4e5917e526246
Kelly Rossmoyer <krossmo@google.com>
2019-08-23 18:01:59 -0700

    rpmsg: glink: Specify source in IRQ name
    Currently, glink-native interrupts from all sources use the same generic
    name and are only distinguishable by number.  To improve diagnosability,
    this change creates a unique name for each glink-native IRQ that also
    specifies its source.
    
    Bug: 138658369
    Test: Ran device, exercised glink-native interrupts, verified naming in
    wakeup logs.
    
    Change-Id: Iff5295b87b52edc3d6cfb2ef906121a418a49a92
    Signed-off-by: Kelly Rossmoyer <krossmo@google.com>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- drivers/rpmsg/qcom_glink_native.c
+++ drivers/rpmsg/qcom_glink_native.c
@@ -116,6 +116,7 @@ struct glink_core_rx_intent {
  * @rx_pipe:	pipe object for receive FIFO
  * @tx_pipe:	pipe object for transmit FIFO
  * @irq:	IRQ for signaling incoming events
+ * @irq_name:	name registered for IRQ
  * @kworker:	kworker to handle rx_done work
  * @task:	kthread running @kworker
  * @rx_work:	worker for handling received control messages
@@ -140,7 +141,7 @@ struct qcom_glink {
 	struct qcom_glink_pipe *tx_pipe;
 
 	int irq;
-	char irq_name[32];
+	const char *irq_name;
 
 	struct kthread_worker kworker;
 	struct task_struct *task;
@@ -1913,11 +1914,14 @@ struct qcom_glink *qcom_glink_native_probe(struct device *dev,
 					   struct qcom_glink_pipe *tx,
 					   bool intentless)
 {
+	static const char *unknown_irq = "unknown";
+	static const char *irq_prefix = "glink-native-";
 	struct qcom_glink *glink;
 	u32 *arr;
 	int size;
 	int irq;
 	int ret;
+	const char *irq_src;
 
 	glink = devm_kzalloc(dev, sizeof(*glink), GFP_KERNEL);
 	if (!glink)
@@ -1946,6 +1950,15 @@ struct qcom_glink *qcom_glink_native_probe(struct device *dev,
 	if (ret < 0)
 		glink->name = dev->of_node->name;
 
+	irq_src = glink->name;
+	if (irq_src == NULL)
+		irq_src = unknown_irq;
+	size = strlen(irq_prefix) + strlen(irq_src) + 1;
+	glink->irq_name = devm_kzalloc(dev, size, GFP_KERNEL);
+	if (!glink->irq_name)
+		return ERR_PTR(-ENOMEM);
+	snprintf((char *)glink->irq_name, size, "%s%s", irq_prefix, irq_src);
+
 	glink->mbox_client.dev = dev;
 	glink->mbox_client.knows_txdone = true;
 	glink->mbox_chan = mbox_request_channel(&glink->mbox_client, 0);
@@ -1971,8 +1984,6 @@ struct qcom_glink *qcom_glink_native_probe(struct device *dev,
 		dev_err(dev, "failed to register early notif %d\n", ret);
 
 	irq = of_irq_get(dev->of_node, 0);
-	snprintf(glink->irq_name, sizeof(glink->irq_name)-1, "glink-native_%s", glink->name);
-	dev_err(dev, "glink-native glink->irq_name=%s irq=%d\n", glink->irq_name, irq);
 	ret = devm_request_irq(dev, irq,
 			       qcom_glink_native_intr,
 			       IRQF_SHARED,
