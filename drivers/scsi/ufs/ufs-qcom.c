39f2129d76db5473caefba085cc1da665200a038
Veerabhadrarao Badiganti <vbadigan@codeaurora.org>
2019-10-29 11:19:49 +0530

    scsi: ufs: Ensure clocks are ON while running power change procedure
    UFS driver is not holding clock requests while performing vendor-specific
    power change settings. This can lead to un-clocked access if the
    context which performs this power change sequence (Eg rls_handler) gets
    scheduled out just before this vendor-specific steps and gets a chance
    to run after clock gating delay. Clock gating can turn off the clock
    during this time and can lead to un-clocked access.
    
    CRs-fixed: 2549002
    Change-Id: I49746ed5aa547271fc96cb33656f5fd9db42d07c
    Signed-off-by: Veerabhadrarao Badiganti <vbadigan@codeaurora.org>

--- drivers/scsi/ufs/ufs-qcom.c
+++ drivers/scsi/ufs/ufs-qcom.c
@@ -1391,6 +1391,9 @@ static int ufs_qcom_pwr_change_notify(struct ufs_hba *hba,
 	int ret = 0;
 	int res = 0;
 
+	hba->ufs_stats.clk_hold.ctx = PWR_CHG_NOTIFY;
+	ufshcd_hold(hba, false);
+
 	if (!dev_req_params) {
 		pr_err("%s: incoming dev_req_params is NULL\n", __func__);
 		ret = -EINVAL;
@@ -1481,6 +1484,8 @@ static int ufs_qcom_pwr_change_notify(struct ufs_hba *hba,
 		break;
 	}
 out:
+	hba->ufs_stats.clk_rel.ctx = PWR_CHG_NOTIFY;
+	ufshcd_release(hba, false);
 	return ret;
 }
 
