38210e55a9c6d1e46c2340b5b0a728045a946191
miui-autobuilder <miui-autobuilder@xiaomi.com>
2019-12-01 18:29:07 +0800

    Kernel: Xiaomi audio-kernel changes for Mi 9 and Redmi K20 Pro Android Q
    The Patch based on QualComm release TAG: LA.UM.8.1.r1-08700-sm8150.0
    
    Change-Id: I538c3a9f7c87d02d49682f36b72843c9d810c994
    Signed-off-by: miui-autobuilder <miui-autobuilder@xiaomi.com>

--- techpack/audio/Android.mk
+++ techpack/audio/Android.mk
@@ -22,6 +22,8 @@ include $(MY_LOCAL_PATH)/soc/Android.mk
 include $(MY_LOCAL_PATH)/asoc/Android.mk
 include $(MY_LOCAL_PATH)/asoc/codecs/Android.mk
 include $(MY_LOCAL_PATH)/asoc/codecs/wcd934x/Android.mk
+include $(MY_LOCAL_PATH)/asoc/codecs/tas2557/Android.mk
+include $(MY_LOCAL_PATH)/asoc/codecs/cs35l41/Android.mk
 endif
 
 ifeq ($(call is-board-platform-in-list,sdm670 msmnile),true)
